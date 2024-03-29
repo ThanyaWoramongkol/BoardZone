9fedac0d55edaafe331c64929218bde896098082
suns <138846871+Suns18@users.noreply.github.com>
2024-03-29 16:25:34 +0700

    Add Tic Tac Toe Game
    GameClient extends JFrame

---
+++ Project OOP/BoardZone/src/GameBoard.java
@@ -0,0 +1,297 @@
+
+public class GameBoard {
+    private char[][] board;
+    private int placed = 0; 
+    private boolean running = false;
+    private int gameState = DRAW;
+    private char playingMark = 'O';
+    private boolean withAI = false;
+    
+    public static final int DRAW = 0;
+    public static final int O_WIN = 1;
+    public static final int X_WIN = 2;
+    
+    public static final char EMPTY = '\u0000';
+    
+    public GameBoard() {
+        board = new char[3][3];
+        running = true;
+    }
+    
+    public void setBoard(char[][] board) {
+        this.board = board;
+    }
+    
+    public char[][] getBoard() {
+        return this.board;
+    }
+    
+    public void setMarkPlaced(int placed) {
+        this.placed = placed;
+    }
+    
+    public int getMarkPlaced() {
+        return this.placed;
+    }
+    
+    public void setRunningState(boolean running) {
+        this.running = running;
+    }
+    
+    public boolean isRunning() {
+        return this.running;
+    }
+    
+    public void setGameState(int gameState) {
+        this.gameState = gameState;
+    }
+    
+    public int getGameState() {
+        return this.gameState;
+    }
+    
+    public void setPlayingMark(char mark) {
+        this.playingMark = mark;
+    }
+    
+    public char getPlayingMark() {
+        return this.playingMark;
+    }
+    
+    public void setWithAI(boolean withAI){
+        this.withAI = withAI;
+    }
+    
+    public boolean getWithAI() {
+        return this.withAI;
+    }
+    
+    public void placeMark(int row, int col) {
+        if (running && placed < 9) {
+            if (board[row][col] == EMPTY){
+                board[row][col] = playingMark;
+                placed++;
+                int winState = checkWin(board, row, col);
+                if (winState != 0 || placed == 9) {
+                    running = false;
+                    if (winState == 1) {
+                        gameState = O_WIN;
+                    } else if (winState == -1) {
+                        gameState = X_WIN;
+                    } else {
+                        gameState = DRAW;
+                    }
+                } else {
+                    if (playingMark == 'O') {
+                        playingMark = 'X';
+                    } else {
+                        playingMark = 'O';
+                    }
+                }
+                if (placed < 9 && withAI && playingMark == 'X') {
+                    moveAI();
+                }
+            }
+        }
+    }
+    
+    public int checkWin(char[][] board, int row, int col) {
+        char mark = board[row][col];
+        //Row Check
+        boolean rowWin = true;
+        for (int c = 0; c < 3; c++) {
+            if (board[row][c] != mark) {
+                rowWin = false;
+                break;
+            }
+        }
+        
+        //Column Check
+        boolean colWin = true;
+        for (int r = 0; r < 3; r++) {
+            if (board[r][col] != mark) {
+                colWin = false;
+                break;
+            }
+        }
+        
+        //Diagonal check
+        boolean diagLRWin = false;
+        if (row == col) { 
+            diagLRWin = true;
+            for (int i = 0; i < 3; i++) {
+                if (board[i][i] != mark) {
+                    diagLRWin = false;
+                }
+            }
+        }
+        boolean diagRLWin = false;
+        if (row == 2 - col) {
+            diagRLWin = true;
+            for (int i = 0; i < 3; i++) {
+                if (board[i][2 - i] != mark) {
+                    diagRLWin = false;
+                }
+            }
+        }
+        
+        if (rowWin || colWin || diagLRWin || diagRLWin) {
+            if (mark == 'O') {
+                return 1;
+            } else {
+                return -1;
+            }
+        } else {
+            return 0;
+        }
+    }
+    
+    public void showBoard() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != EMPTY){
+                    System.out.print(board[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println("");
+            if (i < 2) {
+                System.out.println("-----");                
+            }
+        }
+    }
+    
+    public void showBoard(int[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != EMPTY){
+                    System.out.print(board[i][j]);
+                } else {
+                    System.out.print("0");
+                }
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println("");
+            if (i < 2) {
+                System.out.println("-----");                
+            }
+        }
+    }
+    
+    public void showBoard(char[][] board) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] != EMPTY){
+                    System.out.print(board[i][j]);
+                } else {
+                    System.out.print(" ");
+                }
+                if (j < 2) {
+                    System.out.print("|");
+                }
+            }
+            System.out.println("");
+            if (i < 2) {
+                System.out.println("-----");                
+            }
+        }
+    }
+    
+    public void newGame(boolean withAI) {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j] = EMPTY;
+            }
+        }
+        running = true;
+        placed = 0;
+        playingMark = 'O';
+        this.withAI = withAI;
+        
+    }
+    
+    public void moveAI() {
+        int[][] testTable = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                testTable[i][j] = 10;
+            }
+        }
+        char[][] tempBoard = new char[3][3];
+        for (int i = 0; i < 3; i++) {
+            System.arraycopy(board[i], 0, tempBoard[i], 0, 3);
+        }
+        int minVal = Integer.MAX_VALUE;
+        int val;
+        int bestRow = 0, bestCol = 0;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (tempBoard[i][j] == EMPTY) {
+                    tempBoard[i][j] = 'X';
+                    val = miniMax(tempBoard, i, j, placed + 1, true);
+                    tempBoard[i][j] = EMPTY;
+                    testTable[i][j] = val;
+                    if (val < minVal) {
+                        minVal = val;
+                        bestRow = i;
+                        bestCol = j;
+                    }
+                }
+            }
+        }
+        showBoard(testTable);
+//        System.out.println(bestRow +", " +bestCol + " " + minVal);
+        placeMark(bestRow, bestCol);
+    }
+    
+    private int miniMax(char[][] board, int row, int col, int depth, boolean maxTurn) {
+        int winState = checkWin(board, row, col);
+        
+        if (depth == 9 || (depth < 9 && winState != 0)) {
+           return winState * depth;
+        }
+        
+        if (maxTurn) {
+            int maxVal = -2;
+            int val;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == EMPTY) {
+                        board[i][j] = 'O';
+                        val = miniMax(board, i, j, depth + 1, false);
+                        board[i][j] = EMPTY;
+                        if (val > maxVal){
+                            maxVal = val;
+                        }
+                    }
+                }
+            }
+            return maxVal;
+        } else {
+            int minVal = 2;
+            int val;
+            for (int i = 0; i < 3; i++) {
+                for (int j = 0; j < 3; j++) {
+                    if (board[i][j] == EMPTY) {
+                        board[i][j] = 'X';
+                        val = miniMax(board, i, j, depth + 1, true);
+                        board[i][j] = EMPTY;
+                        if (val < minVal){
+                            minVal = val;
+                        }
+                    }
+                }
+            }
+            return minVal;
+        }
+    }
+    
+    
+    
+}
---
+++ Project OOP/BoardZone/src/GameClient.java
@@ -0,0 +1,183 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+public class GameClient extends JFrame implements ActionListener, MouseListener, Runnable {
+    private JPanel gamePanel, gameState, newGamePanel;
+    private JLabel[][] marks;
+    private JLabel playerText, newGameText;
+    private JButton newGameWithAIBtn, newGameWithoutAIBtn;
+    private GameBoard game;
+    
+    public static final Color MENU_COLOR = new Color(42, 43, 43);
+    public static final Color BG_COLOR = new Color(58, 56, 56);
+    public static final Color BORDER_COLOR = new Color(75, 75, 75);
+    public static final Color FONT_COLOR = new Color(202, 202, 202);
+    public static final Color O_COLOR = new Color(68, 117, 201);
+    public static final Color X_COLOR = new Color(222, 103, 103);
+    public static final String fontName = "Source Code Pro";
+    
+    public GameClient() {
+        gamePanel = new JPanel();
+        gameState = new JPanel();
+        newGamePanel = new JPanel();
+        marks = new JLabel[3][3];
+        playerText = new JLabel();
+        newGameText = new JLabel("New Game : ");
+        newGameWithAIBtn = new JButton("Against AI");
+        newGameWithoutAIBtn = new JButton("2 Players");
+        game = new GameBoard();
+        
+        this.add(gamePanel);
+        gamePanel.setBackground(BG_COLOR);
+        gamePanel.setPreferredSize(new Dimension(600, 600));
+        gamePanel.setLayout(new GridLayout(3, 3));
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                marks[i][j] = new JLabel();
+                marks[i][j].setBackground(BG_COLOR);
+                marks[i][j].setBorder(BorderFactory.createLineBorder(BORDER_COLOR, 3));
+                marks[i][j].setHorizontalAlignment(JLabel.CENTER);
+                marks[i][j].setVerticalAlignment(JLabel.CENTER);
+                marks[i][j].setFont(new Font(fontName, Font.BOLD, 128));
+                gamePanel.add(marks[i][j]);
+            }
+        }
+        
+        gamePanel.addMouseListener(this);
+        
+        this.add(gameState, BorderLayout.NORTH);
+        gameState.add(playerText);
+        playerText.setFont(new Font(fontName, Font.PLAIN, 32));
+        playerText.setForeground(FONT_COLOR);
+        gameState.setBackground(MENU_COLOR);
+        
+        this.add(newGamePanel, BorderLayout.SOUTH);
+        newGamePanel.add(newGameText);
+        newGameText.setFont(new Font(fontName, Font.PLAIN, 32));
+        newGameText.setForeground(FONT_COLOR);
+        newGamePanel.add(newGameWithAIBtn);
+        newGameWithAIBtn.setFont(new Font(fontName, Font.PLAIN, 24));
+        newGameWithAIBtn.addActionListener(this);
+        newGameWithAIBtn.setForeground(FONT_COLOR);
+        newGameWithAIBtn.setBackground(BG_COLOR);
+        newGameWithAIBtn.setBorderPainted(false);
+        newGamePanel.setBackground(MENU_COLOR);
+        newGamePanel.add(newGameWithoutAIBtn);
+        newGameWithoutAIBtn.setFont(new Font(fontName, Font.BOLD, 24));
+        newGameWithoutAIBtn.addActionListener(this);
+        newGameWithoutAIBtn.setForeground(FONT_COLOR);
+        newGameWithoutAIBtn.setBackground(BORDER_COLOR);
+        newGameWithoutAIBtn.setBorderPainted(false);
+        newGamePanel.setBackground(MENU_COLOR);
+        
+        Thread t = new Thread(this);
+        t.start();
+    }
+
+    public void updatePosition(int row, int col) {
+        char[][] board = game.getBoard();
+        if (board[row][col] == 'O') {
+            marks[row][col].setForeground(O_COLOR);
+            marks[row][col].setText("O");
+        } else if (board[row][col] == 'X') {
+            marks[row][col].setForeground(X_COLOR);
+            marks[row][col].setText("X");
+        } else {
+            marks[row][col].setText("");
+        }
+    }
+    
+    public void updateAll() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                updatePosition(i, j);
+            }
+        }
+    }
+    
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int posX = e.getX() * 3 / gamePanel.getWidth();
+        int posY = e.getY() * 3 / gamePanel.getHeight();
+        game.placeMark(posY, posX);
+//        if (game.getMarkPlaced() < 8) {
+//            game.moveAI();
+//        }
+        this.updateAll();
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {}
+
+    @Override
+    public void mouseReleased(MouseEvent e) {}
+
+    @Override
+    public void mouseEntered(MouseEvent e) {}
+
+    @Override
+    public void mouseExited(MouseEvent e) {}
+
+    @Override
+    public void run() {
+        while (true) {
+            if (!game.getWithAI()){
+                if (game.isRunning()) {
+                    char playingMark = game.getPlayingMark();
+                    if (playingMark == 'O') {
+                        playerText.setText("O's Turn");
+                    } else {
+                        playerText.setText("X's Turn");
+                    }
+                    updateAll();
+                } else {
+                    int winState = game.getGameState();
+                    if (winState == 0) {
+                        playerText.setText("Draw!");
+                    } else if (winState == 1) {
+                        playerText.setText("O Win!");
+                    } else {
+                        playerText.setText("X Win!");
+                    }
+                }
+            } else {
+                if (game.isRunning()) {
+                    char playingMark = game.getPlayingMark();
+                    if (playingMark == 'O') {
+                        playerText.setText("Your turn...");
+                    }
+                    updateAll();
+                } else {
+                    int winState = game.getGameState();
+                    if (winState == 0) {
+                        playerText.setText("Draw!");
+                    } else if (winState == 1) {
+                        playerText.setText("You Win!");
+                    } else {
+                        playerText.setText("AI Win!");
+                    }
+                }
+            }
+        }
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if (e.getSource().equals(newGameWithAIBtn)) {
+            game.newGame(true);
+            newGameWithAIBtn.setFont(new Font(fontName, Font.BOLD, 24));
+            newGameWithoutAIBtn.setFont(new Font(fontName, Font.PLAIN, 24));
+            newGameWithAIBtn.setBackground(BORDER_COLOR);
+            newGameWithoutAIBtn.setBackground(BG_COLOR);
+            updateAll();
+        } else if (e.getSource().equals(newGameWithoutAIBtn)) {
+            game.newGame(false);
+            newGameWithoutAIBtn.setFont(new Font(fontName, Font.BOLD, 24));
+            newGameWithAIBtn.setFont(new Font(fontName, Font.PLAIN, 24));
+            newGameWithAIBtn.setBackground(BG_COLOR);
+            newGameWithoutAIBtn.setBackground(BORDER_COLOR);
+            updateAll();
+        }
+    }
+    
+}
