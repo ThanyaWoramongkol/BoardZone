143883dddd1fa589e0b4787e6c5033e71b48c619
Supanut Arun <139570148+Demon720AA@users.noreply.github.com>
2024-04-02 09:40:05 +0700

    comment

--- Project OOP/BoardZone/src/Pay.java
+++ Project OOP/BoardZone/src/Pay.java
@@ -1,12 +1,3 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-
-/**
- *
- * @author pc
- */
 import java.awt.*;
 import java.awt.event.*;
 import java.io.File;
